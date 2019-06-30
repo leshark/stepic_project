edf0aae390a5e224cfce1501057129019c7ee33a
leshark <al.pyrko@yandex.ru>
2019-06-30 17:44:01 +0300

    initial commit, added main page test

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import sys
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        br = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        br = webdriver.Firefox(firefox_profile=fp)
+
+    else:
+        print("This browser is not supported, test abortion... ")
+        sys.exit(1)
+
+    yield br
+
+    print("\nquit browser..")
+    br.quit()
